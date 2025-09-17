29b8c0134fd6a07170d1f7184d9eae76872b6778
Hosana FG <fernandeshosana48@gmail.com>
2025-09-17 09:55:47 -0300

    Atualizando pasta Lista02 e removendo arquivos desnecessários

--- Lista01/questao23.c
+++
@@ -1,3 +0,0 @@
-/*23. Escreva um programa que calcule o produto entre um valor dado x por 2 elevado a um valor dado n.
-Obs.: Utilize operadores bin ́arios.*/
-
--- README.md
+++
@@ -1,3 +0,0 @@
-# labprog
-
-Questões das listas 01 e 02 da disciplina de Laboratório de programação 2025.2 IFCE Maracanaú
