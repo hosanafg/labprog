4ab4b350db994d75785c2033bf153a96d19fb99e
hosanafg <fernandeshosana48@gmail.com>
2025-11-17 18:57:44 -0300

    Segunda correção da questão 3

--- lista04/questao03.c
+++ lista04/questao03.c
@@ -1,8 +1,7 @@
 /*3. Escreva um programa que solicite iterativamente um n ́umero do usu ́ario e imprima sempre o menor
 valor fornecido. Crie um crit ́erio para finaliza ̧c ̃ao do programa. Utilize ponteiros.*/
-
-#define TAM 5
 #include <stdio.h>
+#include <limits.h>
 
 int menorNumero(const char *msg);
 
@@ -16,21 +15,22 @@ int main () {
 }
 
 int menorNumero(const char *msg) {
-    int numeroLido,menorNumero;
+    int numeroLido;
     int leituraScanf;
-
-    puts (msg);
-    leituraScanf = scanf("%d", &numeroLido);
-    menorNumero=numeroLido;
+    int menorNumero = INT_MAX;
 
     do {
+        puts (msg);
         leituraScanf = scanf("%d", &numeroLido);
         if (leituraScanf!=1) {
             puts("Entrada inválida! ");
             while (getchar() != '\n'); 
             continue;
-        } if (numeroLido%3==0) break;
-        if (numeroLido < menorNumero) menorNumero=numeroLido;
+        } if (numeroLido < menorNumero) menorNumero=numeroLido;
+        if (numeroLido%3==0) {
+            if (menorNumero == INT_MAX) return numeroLido;
+            break;
+        } 
     } while (1);
     
     return menorNumero;
