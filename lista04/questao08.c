eb8ef1524c55683d88dbae85cdcecef2acbbfacf
hosanafg <fernandeshosana48@gmail.com>
2025-11-17 17:15:31 -0300

    Corrigindo questão 8

--- lista04/questao08.c
+++ lista04/questao08.c
@@ -5,6 +5,24 @@
 #include <stdlib.h>
 #define TAM 50
 
+int buscarCaractere(const char *lista,char letra);
+void preencherVetor (char *lista,const char *msg);
+
+int main () {
+    char vetor[TAM];
+    char x;
+    preencherVetor(vetor,"Digite a palavra/frase:  ");
+    puts("Digite a letra para busca:  ");
+    scanf(" %c",&x);
+    
+    int qtd=0;
+    qtd=buscarCaractere(vetor,x);
+
+    printf("A letra %c aparece %d vezes em '%s'\n",x,qtd,vetor);
+    return 0;
+}
+
+
 int buscarCaractere(const char *lista,char letra) {
     int contador=0;
     while(*lista!='\0') {
@@ -17,18 +35,4 @@ void preencherVetor (char *lista,const char *msg) {
     puts(msg);
     fgets(lista,TAM,stdin);
     lista[strcspn(lista,"\n")]=0;
-}
-
-int main () {
-    char vetor[TAM];
-    char x;
-    preencherVetor(vetor,"Digite a palavra/frase:  ");
-    puts("Digite a letra para busca:  ");
-    scanf("%c",&x);
-    
-    int qtd=0;
-    qtd=buscarCaractere(vetor,x);
-
-    printf("A letra %c aparece %d vezes em '%s'\n",x,qtd,vetor);
-    return 0;
 }
