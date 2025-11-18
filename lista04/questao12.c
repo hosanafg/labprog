1f7d630aaeee203dabf56681a88e670112f6fabb
hosanafg <fernandeshosana48@gmail.com>
2025-11-18 20:36:54 -0300

    Corrigindo questões

--- lista04/questao12.c
+++ lista04/questao12.c
@@ -3,6 +3,38 @@ onde r  ́e um n ́umero pseudoaleat ́orio, xi e yi correspondem ao i-esimo ele
 respectivamente. Considere os valores limites definidos na quest ̃ao anterior para yi, caso o valor
 obtido a partir de xi esteja fora da faixa.*/
 
+#define TAM 10
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void gerarRand (int *vetor, int max, const char *msg);
+void matrizOcorrencias(const int *vetorA, const int *vetorB, int randmaximo, int matrizFreq[][randmaximo], int tamanho);
+void imprimirMatriz (int randmaximo, int matrizFreq[][randmaximo]);
+
+int main () {
+    int vetorX[TAM];
+    int vetorY[TAM];
+    int valorMaximo;
+
+    puts("Digite um numero limite p/ o intervalo de nums. aleatórios:  ");
+    scanf("%d",&valorMaximo);
+
+    gerarRand(vetorX,valorMaximo,"Vetor X: ");
+    
+    return 0;
+}
+
+void gerarRand (int *vetor, int max,const char *msg) {
+    int valor_aleatorio;
+    puts(msg);
+    for (int i=0;i<TAM;i++){
+        valor_aleatorio=(rand()%max);
+        vetor[i]=valor_aleatorio;
+        printf("%d  ",vetor[i]);
+    }
+}
+
 /*     yi =
 xi if 0 < r ≤ 3;
 xi − 1 if 3 < r ≤ 5
@@ -11,26 +43,23 @@ xi − 2 if 8 < r ≤ 9
 xi + 2 if 9 < r ≤ 10
 */
 
-#define TAM 10
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-
-void gerarRand (int *vetor, int max);
-
-int main () {
-
+void matrizOcorrencias(const int *vetorA, const int *vetorB, int randmaximo, int matrizFreq[][randmaximo], int tamanho) { 
+    for (int i=0; i<randmaximo; i++) {
+        for (int j=0; j<randmaximo; j++) {
+            matrizFreq[i][j] = 0;
+        }
+    } for (int i=0; i<tamanho; i++) {
+        if (vetorA[i] < randmaximo && vetorB[i] < randmaximo) {
+            matrizFreq[vetorA[i]][vetorB[i]]++;
+        }
+    }
 }
 
-void gerarRand (int *vetor, int max) {
-    srand(time(NULL));
-    int valor_aleatorio;
-
-    puts("VETOR X: ");
-    for (int i=0;i<TAM;i++){
-        valor_aleatorio=(rand()%max);
-        vetor[i] = valor_aleatorio;
-        printf("%d  ",vetor[i]);
+void imprimirMatriz (int randmaximo, int matrizFreq[][randmaximo]) {
+    for (int i=0; i<randmaximo; i++) { 
+        for (int j=0; j<randmaximo; j++) { 
+            printf("%3d  ", matrizFreq[i][j]); 
+        } puts("\n");
     }
 }
 
--- lista04/questão09.c
+++ lista04/questão09.c
@@ -2,6 +2,56 @@
 
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
-void preencherVetor ();
-void ordenarVetor ();
+#define TAM 5
+
+void preencherVetor (int *vetor,int maximo);
+void bubbleSort (int *vetor, int tamanho);
+void imprimirVetor (int *vetor, int tamanho);
+
+int main () {
+    int lista[TAM];
+    int valorMax;
+    puts("Digite um valor máximo para o intervalo de nums. aleatórios:  ");
+    scanf("%d",&valorMax);
+
+    preencherVetor(lista,valorMax);
+    puts("ANTES da ordenação:");
+    imprimirVetor(lista,TAM);
+
+    bubbleSort(lista,TAM);
+    puts("\nAPÓS a ordenação:");
+    imprimirVetor(lista,TAM);
+
+    return 0;
+}
+
+void preencherVetor (int *vetor,int maximo) {
+    srand(time(NULL));
+    int valor_aleatorio;
+    for (int i=0;i<TAM;i++){
+        valor_aleatorio=(rand()%maximo);
+        vetor[i]=valor_aleatorio;
+    }
+}
+
+void imprimirVetor (int *vetor, int tamanho) {
+    for (int i=0;i<tamanho;i++) {
+        printf("%d ",vetor[i]);
+    }
+}
+
+void bubbleSort (int *vetor, int tamanho) {
+    int aux;
+    int *pVetor=vetor;
+    for (int i=0;i<tamanho;i++) {
+        for (int j=0; j<tamanho-1-i; j++) {
+            if (*(pVetor+j) > *(pVetor+j+1)) {
+                aux = *(pVetor+j);
+                *(pVetor+j) = *(pVetor+j+1);
+                *(pVetor+j+1)=aux;
+            }      
+        }
+    }
+}
