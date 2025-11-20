93295e4d47796b91527cec909c6af8b0e778b641
hosanafg <fernandeshosana48@gmail.com>
2025-11-20 14:36:34 -0300

    questao2

--- lista05/questao02.c
+++ lista05/questao02.c
@@ -1,29 +1,41 @@
 /*Modifique a quest ̃ao anterior criando uma fun ̧c ̃ao que armazena em um vetor os endere ̧cos do
 primeiro e do  ́ultimo elemento PAR do vetor.*/
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #define TAM 10
 
 void gerarRand (int *vetor, const int max, const int tamanho);
 void imprimirVetor (int *vetor, const int tamanho);
-int *buscaPares(const int *vetor,const int tamanho);
+int *buscaPrimeiroPar(const int *vetor,const int tamanho);
+int *buscaUltimoPar(const int *vetor,const int tamanho);
 int encontrarPosicaoPrimeiroPar(const int *vetor, const int tamanho);
+int encontrarPosicaoUltimoPar(const int *vetor, const int tamanho);
 
 int main () {
     int lista[TAM];
     int vMax;
     int *primeiroPar=NULL;
+    int *ultimoPar=NULL;
 
     puts("Qual o valor máximo para gerar os números aleatórios?:  ");
     scanf("%d",&vMax);
 
     gerarRand(lista,vMax,TAM);
     imprimirVetor(lista,TAM);
-    primeiroPar=buscaPares(lista,TAM);
+
+    primeiroPar=buscaPrimeiroPar(lista,TAM);
+    ultimoPar=buscaUltimoPar(lista,TAM);
+
     int posPrimeiroPar=encontrarPosicaoPrimeiroPar(lista,TAM);
+    int posUltimoPar=encontrarPosicaoUltimoPar(lista,TAM);
+
     puts("\nPrimeiro número par do vetor: ");
     printf("[0x%p]: lista [%d] = %d",primeiroPar,posPrimeiroPar,*primeiroPar);
+    puts("\nÚltimo número par do vetor: ");
+    printf("[0x%p]: lista [%d] = %d",ultimoPar,posUltimoPar,*ultimoPar);
+    
     return 0;
 }
 
@@ -32,7 +44,7 @@ void gerarRand (int *vetor, const int max, const int tamanho) {
     srand(time(NULL));
     for (int i=0;i<tamanho;i++){
         valor_aleatorio=(rand()%max);
-        vetor[i]=valor_aleatorio;
+        *(vetor+i)=valor_aleatorio;
     }
 }
 void imprimirVetor (int *vetor, const int tamanho) {
@@ -41,7 +53,7 @@ void imprimirVetor (int *vetor, const int tamanho) {
         printf("%d ",vetor[i]);
     }
 }
-int *buscaPares(const int *vetor,const int tamanho) {
+int *buscaPrimeiroPar(const int *vetor,const int tamanho) {
     const int *p=vetor;
     for (int i=0;i<tamanho;i++) {
         if (*(p+i)%2 == 0) {
@@ -49,7 +61,21 @@ int *buscaPares(const int *vetor,const int tamanho) {
         }
     } return NULL;
 }
+int *buscaUltimoPar(const int *vetor,const int tamanho) {
+    const int *p=vetor;
+    for (int i=tamanho-1;i>=0;i++) {
+        if (*(p+i)%2 == 0) {
+            return (int *)(p+i);
+        }
+    } return NULL;
+}
 int encontrarPosicaoPrimeiroPar(const int *vetor, const int tamanho) {
     for (int i=0;i<tamanho;i++) {
-        if (vetor[i] % 2 == 0) return i;
+        if (*(vetor+i) % 2 == 0) return i;
     } return -1;
+}
+int encontrarPosicaoUltimoPar(const int *vetor, const int tamanho) {
+    for (int i=tamanho-1;i>=0;i--) {
+        if (*(vetor+i) % 2 == 0) return i;
+    } return -1;
+}
