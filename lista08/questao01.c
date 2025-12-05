cabed75fcf1fe7b1fc3928b7b1ef13902ea0a24f
hosanafg <fernandeshosana48@gmail.com>
2025-12-04 22:46:16 -0300

    add lista 8

---
+++ lista08/questao01.c
@@ -0,0 +1,6 @@
+/*1. Escreva um programa em C que leia dados de diferentes usu ́arios e grave em um arquivo csv.
+Pesquise sobre a organiza ̧c ̃ao de um arquivo csv.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
