9628959c5abb340917c6db29466204a507c09ede
hosanafg <fernandeshosana48@gmail.com>
2025-12-04 22:47:26 -0300

    add questao 2

---
+++ lista08/questao02.c
@@ -0,0 +1,8 @@
+/*2. Vocˆe  ́e o dono de uma loja de materiais e precisa manter um invent ́ario que poder ́a informar quais
+e quantas ferramentas vocˆe possui e o custo de cada uma. Escreva um programa que inicialiaze
+o arquivo “hardware.dat” para 100 registros vazios, e permita que vocˆe insira os dados referentes
+a cada ferramenta, lista todas as suas ferramentas, exclua um registro para a ferramenta que n ̃ao
+possui mais e atualize qualquer informa ̧c ̃ao no arquivo. O n ́umero de identifica ̧c ̃ao da ferramenta
+dever ́a ser o n ́umero do registro.
+Vocˆe deve armazenar em seu arquivo os seguintes campos.
+# registro - Nome da ferramenta - Quantidade - Custo*/
